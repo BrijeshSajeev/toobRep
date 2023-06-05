d70e765b146a386a71654e0264102eb36b2721c0
BrijeshSajeev <brijesh00300@gmail.com>
2023-06-05 22:07:54 +0530

    Prototype Scope

--- 02-spring-boot-core/06-Scope-of-bean/src/main/java/com/brijesh/springCoreDemo/rest/DemoRestController.java
+++ 02-spring-boot-core/06-Scope-of-bean/src/main/java/com/brijesh/springCoreDemo/rest/DemoRestController.java
@@ -2,11 +2,15 @@ package com.brijesh.springCoreDemo.rest;
 
  import com.brijesh.springCoreDemo.common.Coach;
  import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.beans.factory.annotation.Configurable;
  import org.springframework.beans.factory.annotation.Qualifier;
+ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+ import org.springframework.context.annotation.Scope;
  import org.springframework.web.bind.annotation.GetMapping;
  import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class DemoRestController {
 
     private Coach myCoach;
