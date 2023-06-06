6e4c17f358b4a29fbb382a2be8b0299465a9f513
BrijeshSajeev <brijesh00300@gmail.com>
2023-06-06 19:59:00 +0530

    CRUD Starter🚀

--- 03-hibernate-jpa-CRUD/01-CRUD-demo-starter/src/main/java/com/springboot/cruddemo/CruddemoApplication.java
+++ 03-hibernate-jpa-CRUD/01-CRUD-demo-starter/src/main/java/com/springboot/cruddemo/CruddemoApplication.java
@@ -3,6 +3,7 @@ package com.springboot.cruddemo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -10,6 +11,8 @@ public class CruddemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
+
+		@Bean
 		public CommandLineRunner CommandLineRunner(){
 			return runner->{
 			System.out.println("Hello world");
