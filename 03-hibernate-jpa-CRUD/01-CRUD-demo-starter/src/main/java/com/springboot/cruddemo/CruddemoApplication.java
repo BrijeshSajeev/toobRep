edfd9ce56a21d286686360db425196236b3765e6
BrijeshSajeev <brijesh00300@gmail.com>
2023-06-06 19:57:55 +0530

    CRUD Starter

---
+++ 03-hibernate-jpa-CRUD/01-CRUD-demo-starter/src/main/java/com/springboot/cruddemo/CruddemoApplication.java
@@ -0,0 +1,19 @@
+package com.springboot.cruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+		public CommandLineRunner CommandLineRunner(){
+			return runner->{
+			System.out.println("Hello world");
+			};
+
+		}
+}
