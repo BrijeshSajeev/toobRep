2fa1997dcaf61bf02a0f99e0c7d52026693a0f8f
BrijeshSajeev <brijesh00300@gmail.com>
2023-06-14 22:41:29 +0530

    🚀spring data rest

--- 04-spring-rest-curd/04-spring-crud-emp-spring-data-REST/src/main/java/com/example/springboot/crudDemo/dao/EmployeeRepository.java
+++ 04-spring-rest-curd/04-spring-crud-emp-spring-data-REST/src/main/java/com/example/springboot/crudDemo/dao/EmployeeRepository.java
@@ -2,9 +2,10 @@ package com.example.springboot.crudDemo.dao;
 
 import com.example.springboot.crudDemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 
-@RepositoryRestController(path = "members")
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 //    Nothing but Custom Methods
 }
