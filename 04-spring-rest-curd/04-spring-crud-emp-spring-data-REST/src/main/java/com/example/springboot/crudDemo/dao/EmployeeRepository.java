5d9e02937316e11927dcbfa5e4c5404295609486
BrijeshSajeev <brijesh00300@gmail.com>
2023-06-14 22:36:11 +0530

    customising the PATH

--- 04-spring-rest-curd/04-spring-crud-emp-spring-data-REST/src/main/java/com/example/springboot/crudDemo/dao/EmployeeRepository.java
+++ 04-spring-rest-curd/04-spring-crud-emp-spring-data-REST/src/main/java/com/example/springboot/crudDemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.example.springboot.crudDemo.dao;
 
 import com.example.springboot.crudDemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+@RepositoryRestController(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 //    Nothing but Custom Methods
 }
