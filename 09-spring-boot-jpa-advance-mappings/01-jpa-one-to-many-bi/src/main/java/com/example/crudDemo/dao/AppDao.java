44a6db0fd11399017d4185d9c1ccc5c80d4b4987
BrijeshSajeev <brijesh00300@gmail.com>
2023-07-01 21:48:17 +0530

    OneToMany Mapping

---
+++ 09-spring-boot-jpa-advance-mappings/01-jpa-one-to-many-bi/src/main/java/com/example/crudDemo/dao/AppDao.java
@@ -0,0 +1,18 @@
+package com.example.crudDemo.dao;
+
+import com.example.crudDemo.entity.Instructor;
+import com.example.crudDemo.entity.InstructorDetail;
+
+public interface AppDao {
+    public void save(Instructor theInstructor);
+    public void saveByInsDetail(InstructorDetail theInstructor);
+    public Instructor findInstructorById(int theId);
+
+    public InstructorDetail findInstructorDetailById(int theId);
+
+    public void deleteInstructorById(int theId);
+
+    public void deleteInstructorDetailById(int theId);
+
+
+}
---
+++ 09-spring-boot-jpa-advance-mappings/01-jpa-one-to-many-bi/src/main/java/com/example/crudDemo/dao/AppDaoImpl.java
@@ -0,0 +1,58 @@
+package com.example.crudDemo.dao;
+
+import com.example.crudDemo.entity.Instructor;
+import com.example.crudDemo.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AppDaoImpl implements AppDao{
+
+
+    @Autowired
+    private EntityManager entityManager;
+
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+
+    @Override
+    @Transactional
+    public void saveByInsDetail(InstructorDetail theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+
+    @Override
+    public Instructor findInstructorById(int theId) {
+       Instructor theIns =entityManager.find(Instructor.class,theId);
+        return theIns;
+    }
+
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int theId) {
+        Instructor theIns =entityManager.find(Instructor.class,theId);
+        entityManager.remove(theIns);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int theId) {
+        InstructorDetail theInsDet=entityManager.find(InstructorDetail.class,theId);
+        entityManager.remove(theInsDet);
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int theId) {
+
+        return entityManager.find(InstructorDetail.class,theId);
+    }
+
+
+}
